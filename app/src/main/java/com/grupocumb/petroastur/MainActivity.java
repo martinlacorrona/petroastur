f164400c9785347d46e0a03c8d525e6294145bcd
Martin Fernandez Prieto <tatin1998@gmail.com>
2020-01-10 00:15:31 +0100

    quitado el settings que salia en los puntos arriba a la derecha ya que no tenia sentido

--- app/src/main/java/com/grupocumb/petroastur/MainActivity.java
+++ app/src/main/java/com/grupocumb/petroastur/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
