b220c4f434245778c5d252071dc602f021caf562
Martin Fernandez Prieto <tatin1998@gmail.com>
2020-01-10 00:14:18 +0100

    arreglado el bug que salia un mensaje de la bbdd

--- app/src/main/java/com/grupocumb/petroastur/db/AppDatabase.java
+++ app/src/main/java/com/grupocumb/petroastur/db/AppDatabase.java
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase;
 
 import com.grupocumb.petroastur.model.EstacionServicio;
 
-@Database(entities = {EstacionServicio.class}, version = 1)
+@Database(entities = {EstacionServicio.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract EstacionServicioDao estacionServicioDao();
 }
